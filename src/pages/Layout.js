0c97c6e8cbdb4297379fe7d64a2a31535e1d5b87
Josue Rodriguez <josue.rodriguez.siatama@gmail.com>
2021-07-25 12:24:28 -0500

    probando el host de firebase

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,9 +1,19 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
-const Layout = ({nameComponent, name, page}) =>{
-  
+import {useStaticQuery, graphql} from 'gatsby';
+const Layout = ({nameComponent, name, page, children}) =>{
+  const data = useStaticQuery(graphql`
+    query{
+      site {
+        siteMetadata{
+          title
+        }
+      }
+    }
+  `);
   return(
     <main>
+      <header>{data.site.siteMetadata.title}</header>
       <title> hola gatsby </title>
       <div>
         <p>hola, este es el {nameComponent} de {name}</p> 
@@ -11,7 +21,9 @@ const Layout = ({nameComponent, name, page}) =>{
       <ul>
         <li><Link to="/about"> go to about</Link> </li>
         <li><Link to="/"> go home </Link> </li>
+        <li><Link to="/blogPage"> go to blog </Link> </li>
       </ul>
+      {children}
     </main>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
-import * as React from 'react';
+import * as React from "react";
 import Layout from './Layout';
-import {setBackgroundColor, container} from './styles.module.css';
+import {setBackgroundColor, container, containerImg} from './styles.module.css';
 import {StaticImage} from 'gatsby-plugin-image';
+
+
 const IndexPage = () =>{
   return(
     <main className={setBackgroundColor}>
       <h2> Welcome to indexpage </h2>
       <Layout nameComponent="HomePage" className={container}/> 
-      <StaticImage alt="imagen random" src="https://thelandscapephotoguy.com/wp-content/uploads/2019/01/landscape%20moravia%20gentle.jpg"/>
-    
+      <StaticImage alt="imagen random" src="https://thelandscapephotoguy.com/wp-content/uploads/2019/01/landscape%20moravia%20gentle.jpg" className={containerImg}/>
     </main>
   );
 }
